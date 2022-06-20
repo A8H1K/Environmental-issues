88c8f938acdf3f166d0fb61e2903d1374814d9b8
adarsharma-in <hello@adarsharma.in>
2022-06-20 12:19:46 +0530

    fix: Made background transparent

--- js/water-quality-bar-graph.js
+++ js/water-quality-bar-graph.js
@@ -1,12 +1,20 @@
+// Bar Graph representing primary causes of air pollution
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
+  // Data to be represented in Bar graph
   var data = google.visualization.arrayToDataTable([
     ["Country", "% purity", { role: "style" }],
     ["Finland", 100, "#b87333"],
     ["US", 85, "silver"],
     ["Japan", 85, "gold"],
     ["France", 70, "color: #e5e4e2"],
+    ["India", 65, "color: #b87333"],
+    ["singapore", 60, "silver"],
+    ["Canada", 50, "gold"],
+    ["Australia", 50, "color: #e5e4e2"],
+    ["Serbia", 45, "color: #b87333"],
+    ["Hong Kong", 30, "silver"],
   ]);
 
   var view = new google.visualization.DataView(data);
@@ -17,15 +25,20 @@ function drawChart() {
     2,
   ]);
 
+  //Attributes of the Bar Graph
   var options = {
-    title: "Density of Precious Metals, in g/cm^3",
+    title: "Water Quality across different nations",
     width: 600,
     height: 400,
     bar: { groupWidth: "95%" },
     legend: { position: "none" },
+    backgroundColor: "transparent",
   };
   var chart = new google.visualization.ColumnChart(
     document.getElementById("columnchart_values")
   );
+  //Generating HTML & CSS based on data & attributes through google API
   chart.draw(view, options);
 }
+
+// Bar Graph ends
