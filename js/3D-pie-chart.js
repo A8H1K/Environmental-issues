f139d0eea909b4fb4de0ec15539c2f20aa21c725
adarsharma-in <hello@adarsharma.in>
2022-06-20 11:23:35 +0530

    feat: Add charts

---
+++ js/3D-pie-chart.js
@@ -0,0 +1,32 @@
+// 3D pie chart representing primary causes of air pollution
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  // Data to be represented in pie chart
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Dust & Constrution", 45],
+    ["Waste Burning", 17],
+    ["Transport", 14],
+    ["Diesel generator", 9],
+    ["Industries", 8],
+    ["Domestic Cooking", 7],
+  ]);
+
+  //Attributes of the pie chart
+  var options = {
+    title: "Activities contrbuting to air pollution",
+    is3D: true,
+    legend: { position: "bottom" },
+    backgroundColor: "transparent",
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart_3d") //Targeting div with id: piechart_3d (in index.html)
+  );
+
+  //Generating HTML & CSS based on data & attributes through google API
+  chart.draw(data, options);
+}
+
+// 3D pie chart ends
---
+++ js/water-quality-bar-graph.js
@@ -0,0 +1,31 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Country", "% purity", { role: "style" }],
+    ["Finland", 100, "#b87333"],
+    ["US", 85, "silver"],
+    ["Japan", 85, "gold"],
+    ["France", 70, "color: #e5e4e2"],
+  ]);
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([
+    0,
+    1,
+    { calc: "stringify", sourceColumn: 1, type: "string", role: "annotation" },
+    2,
+  ]);
+
+  var options = {
+    title: "Density of Precious Metals, in g/cm^3",
+    width: 600,
+    height: 400,
+    bar: { groupWidth: "95%" },
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(
+    document.getElementById("columnchart_values")
+  );
+  chart.draw(view, options);
+}
